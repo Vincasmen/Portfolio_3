98c72723cf3ba7d546c0a921bff7a07f30ffad8e
Vincasmen <vincasmen13@gmail.com>
2022-06-13 23:38:26 +0200

    Sticky nav bar on scroll

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 "use strict";
+
+//  !Dott menu and list
+
 const dottBtn = document.getElementById("dott-icon");
 const headerList = document.getElementById("ul-id");
 
@@ -6,3 +9,22 @@ dottBtn.addEventListener("click", function () {
   headerList.classList.toggle("hidden-ul");
   console.log("BtnClicked");
 });
+
+//  !Sticky scroll nav bar
+
+const navBar = document.getElementById("nav-bar");
+const introDiv = document.getElementById("heading-introduction-div");
+window.onscroll = function () {
+  scrollFunction();
+};
+const sticky = navBar.offsetTop;
+
+function scrollFunction() {
+  if (window.pageYOffset >= sticky) {
+    navBar.classList.add("sticky");
+    introDiv.classList.add("padding");
+  } else {
+    navBar.classList.remove("sticky");
+    introDiv.classList.remove("padding");
+  }
+}
